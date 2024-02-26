32c2a283c6de8b9cd8f20d2e9e72501056c4da0c
Alexander Aseme <aseme@usc.edu>
2024-02-26 13:08:03 -0800

    Finished first few steps of tutorial

--- Start/src/main.cpp
+++ Start/src/main.cpp
@@ -33,7 +33,10 @@ int main(void)
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 
-    glVertexAttribPointer
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
